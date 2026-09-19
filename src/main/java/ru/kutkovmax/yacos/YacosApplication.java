4cfae2e33cb947cda48704e6f840b1ed2429fa3d
Maxim <maximkutkov@gmail.com>
2026-09-19 10:58:32 +0300

    feat: add mdc request id filter

--- src/main/java/ru/kutkovmax/yacos/YacosApplication.java
+++ src/main/java/ru/kutkovmax/yacos/YacosApplication.java
@@ -2,8 +2,12 @@ package ru.kutkovmax.yacos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class YacosApplication {
 
 	public static void main(String[] args) {
